f858eafe686ff02a5f3676d7fe96eb4b3382d515
Hùng Phan <112998342+lamhungphan@users.noreply.github.com>
2025-03-14 12:05:57 +0700

    Alert

--- lib/views/pages/settings_page.dart
+++ lib/views/pages/settings_page.dart
@@ -44,6 +44,28 @@ class _SettingPageState extends State<SettingPage> {
                 },
                 child: Text('Open Snackbar'),
               ),
+              ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text('Alert Content'),
+                        content: Text('Alert Dialog'),
+                        actions: [
+                          FilledButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Text('Close'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: Text('Open Dialog'),
+              ),
               DropdownButton(
                 value: menuItem,
                 items: [
